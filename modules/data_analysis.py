648ea7df30f4c2959130acea9ed9077ce08935a3
Dana Nunez <danaliz98@gmail.com>
2024-10-30 12:13:31 -0700

    finishing features with format

--- modules/data_analysis.py
+++ modules/data_analysis.py
@@ -1,38 +1,56 @@
 import pandas as pd
 import calendar
+from tabulate import tabulate
+
 
 file_path = "../csv_files/sampledata.csv"
 df = pd.read_csv(file_path)
 
-print(df)
-
-# data sample
-#        Date       Category        Description   Amount     Type
-# 0   2024-10-01       Food            Grocery    50.75  Expense
 
 # analyze_spending_by_category
-# def analyze_spending_by_category():
-df_category = df.loc[:, ["Category", "Amount", "Type"]]
-type_expense = df_category[df_category["Type"].isin(["Expense"])]
-spending_by_category = type_expense.groupby("Category")["Amount"].agg(Total_Spending = 'sum')
-spending_by_category.loc['Total']= spending_by_category.sum()
-print(spending_by_category)
+def analyze_spending_by_category():
+    df_category = df.loc[:, ["Category", "Amount", "Type"]]
+    type_expense = df_category[df_category["Type"].isin(["Expense"])]
+    spending_by_category = type_expense.groupby("Category")["Amount"].agg(Total_Spending = 'sum')
+    spending_by_category.loc['Total']= spending_by_category.sum()
+
+    # ANSI escape code \033[<color>m
+    header_color = "\033[33m" # Orange
+    reset = "\033[0m"  # Reset to default
+
+    print("\033[36m -> SPENDING BY CATEGORY  \033[0m")
+    print(tabulate(spending_by_category, headers=[header_color + 'Category' + reset, header_color + 'Total Spending' + reset], tablefmt='pipe'))
 
 
 
 # calculate_average_monthly_spending
-# def calculate_average_monthly_spending():
+def calculate_average_monthly_spending():
 
-df_date = df.loc[:, ["Date", "Amount", "Type"]]
-df_date["Date"] = pd.to_datetime(df_date["Date"])
-df_date["Month"] = df_date["Date"].dt.month.apply(lambda x: calendar.month_abbr[x])
-df_date["Year"] = df_date["Date"].dt.year
+    df_date = df.loc[:, ["Date", "Amount", "Type"]]
+    df_date["Date"] = pd.to_datetime(df_date["Date"])
+    df_date["Month"] = df_date["Date"].dt.month.apply(lambda x: calendar.month_abbr[x])
+    df_date["Year"] = df_date["Date"].dt.year
 
-monthly_spending = df_date.groupby(["Month","Type"])["Amount"].mean()
-print(monthly_spending)
+    df_date_new_arrange = df_date.pivot_table(index=['Year', 'Month'], columns='Type',
+                         values='Amount', aggfunc='mean')
+
+    print("\033[36m -> AVERAGE MONTHLY SPENDING  \033[0m")
+    print(tabulate(df_date_new_arrange, headers=['Month', 'Expense', 'Income'], tablefmt='pipe'))
+
 
 
 # show_top_spending_category
-# def show_top_spending_category():
-top_spending = spending_by_category.sort_values(by="Total_Spending", ascending=False)
-print(top_spending[0:1])
+def show_top_spending_category():
+
+    top_spending = df.groupby(["Category","Type"])["Amount"].sum().reset_index()
+    top_spending = top_spending[top_spending.Type != "Income"]
+    top_spending = top_spending.sort_values(by="Amount", ascending=False).set_index("Category")
+    top_spending = top_spending.loc[:, ["Amount"]]
+
+    print("\033[36m - TOP SPENDING CATEGORY - \033[0m")
+    print(tabulate(top_spending[0:1], headers= ['Category', 'Amount'], tablefmt='pipe'))
+
+
+print(analyze_spending_by_category())
+print(calculate_average_monthly_spending())
+print(show_top_spending_category())
